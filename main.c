d85043b93b39631cb8a1482f51dd2d459ac4209b
zhouZ <zhouzhengfa1995@gmail.com>
2026-05-21 16:09:27 +0800

    a litt

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 // standard library 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
 //Third-party library 
 #include <sqlite3.h>
@@ -11,6 +14,22 @@
 //program entry
 int main(){
     puts("some data and suggestion");
+	char birthday[] = "1995 1 24";
+	printf("My birthday is : %s \n" ,birthday);
+
+	time_t now = time(NULL);
+	struct tm *local  = localtime(&now);
+	int current_year = local->tm_year + 1900 ;
+	int current_month = local->tm_mon +1 ;
+	int current_day = local->tm_mday ;
+	printf("current date is : %d : %d : %d \n" , current_year , current_month ,current_day );
+
+	char birthday_year_str[5] ;
+	strncpy(birthday_year_str ,birthday ,4);
+	birthday_year_str[4] = '\0';
+
+	int birthday_year = atoi(birthday_year_str);
+	printf("get year birthday : %d \n " ,birthday_year );
     return 0;
 }
 
